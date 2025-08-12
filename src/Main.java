44e1214db2393c914fea9582d578db34454f5b8a
Baumzin <brenosuvale@gmail.com>
2025-08-11 21:41:21 -0300

    Finalizando tutorial do arquivo!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,53 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ProdutoDAO dao = new ProdutoDAO();
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("1 - Inserir | 2 - Listar | 3 - Atualizar | 4 - Deletar");
+        int op = scan.nextInt();
+        scan.nextLine(); // limpa o buffer
+
+        switch (op) {
+            case 1:
+                System.out.println("Insira o nome: ");
+                String nome = scan.nextLine();
+                System.out.println("Insira o preço: ");
+                double preco = scan.nextDouble();
+                Produto produto = new Produto(nome, preco);
+                dao.inserir(produto);
+                break;
+            case 2:
+                for (Produto prod : dao.listar()) {
+                    System.out.println(
+                        prod.getId() + " - " +
+                        prod.getNome() + " -  R$" +
+                        prod.getPreco()
+                    );
+                }
+                break;
+            case 3:
+                System.out.println("Insira o ID do produto: ");
+                int id = scan.nextInt();
+                scan.nextLine();
+                System.out.println("Insira o novo nome: ");
+                String nomeNovo = scan.nextLine();
+                System.out.println("Insira o novo preço: ");
+                Double precoNovo = scan.nextDouble();
+                produto = new Produto(nomeNovo, precoNovo);
+                produto.setId(id);
+                dao.atualizar(produto);
+                break;
+            case 4:
+                System.out.println("Insira o ID do produto para deletá-lo");
+                id = scan.nextInt();
+                dao.deletar(id);
+                break;
+            default:
+                System.out.println("Opção inválida");
+                break;
+        }
+        scan.close();
     }
 }
--- src/ProdutoDAO.java
+++ src/ProdutoDAO.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 public class ProdutoDAO {
     public void inserir(Produto produto) {
-        String comandoSql = "INSERTO INTO produtos (nome, preco) VALUES (?, ?)";
+        String comandoSql = "INSERT INTO produtos (nome, preco) VALUES (?, ?)";
 
         try (Connection conexao = Conexao.conectar();
                 PreparedStatement stmt = conexao.prepareStatement(comandoSql)) {
@@ -15,6 +15,53 @@ public class ProdutoDAO {
             stmt.executeUpdate();
             System.err.println("Produto inserido com sucesso!");
         } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Produto> listar() {
+        List<Produto> lista = new ArrayList<>();
+        String comandoSql = "SELECT * FROM produtos";
+
+        try (Connection connection = Conexao.conectar();
+                Statement stmt = connection.createStatement();
+                ResultSet result = stmt.executeQuery(comandoSql)) {
+            while (result.next()) {
+                Produto produto = new Produto(result.getString("nome"), result.getDouble("preco"));
+                produto.setId(result.getInt("id"));
+                lista.add(produto);
+            }
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
+        return lista;
+    }
+
+    public void atualizar(Produto produto) {
+        String comandoSql = "UPDATE produtos SET nome=?, preco=?, WHERE id=?";
+
+        try(Connection connection = Conexao.conectar(); PreparedStatement stmt = connection.prepareStatement(comandoSql)) {
+            stmt.setString(1, produto.getNome());
+            stmt.setDouble(2, produto.getPreco());
+            stmt.setInt(3, produto.getId());
+            stmt.executeUpdate();
+
+            System.out.println("Produto atualizado com sucesso!");
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void deletar(int id){
+        String comandoSql = "DELETE FROM produtos WHERE id=?";
+
+        try(Connection connection = Conexao.conectar(); PreparedStatement stmt = connection.prepareStatement(comandoSql)) {
+
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+
+            System.out.println("Produto deletado com sucesso!");
+        }catch(SQLException e){
             e.printStackTrace();
         }
     }
